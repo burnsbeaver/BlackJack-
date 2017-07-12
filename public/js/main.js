15f31af50a8203181a213e027435b56b808cb649
burnsbeaver <burnsbeaver@gmail.com>
2017-07-12 12:28:43 -0400

    Created deckOfCards Array

---
+++ public/js/main.js
@@ -0,0 +1,263 @@
+var deckOfCards = [
+  {
+    cardType: 2
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 3
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 4
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 5
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 6
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 7
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 8
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 9
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 10
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: "Jack"
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: "Queen"
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: "King"
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: "Ace"
+    cardSuite: "Hearts"
+    CardImage:
+  }
+  {
+    cardType: 2
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 3
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 4
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 5
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 6
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 7
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 8
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 9
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 10
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: "Jack"
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: "Queen"
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: "King"
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: "Ace"
+    cardSuite: "Diamonds"
+    CardImage:
+  }
+  {
+    cardType: 2
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 3
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 4
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 5
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 6
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 7
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 8
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 9
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 10
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: "Jack"
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: "Queen"
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: "King"
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: "Ace"
+    cardSuite: "Clubs"
+    CardImage:
+  }
+  {
+    cardType: 2
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 3
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 4
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 5
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 6
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 7
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 8
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 9
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: 10
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: "Jack"
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: "Queen"
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: "King"
+    cardSuite: "Spades"
+    CardImage:
+  }
+  {
+    cardType: "Ace"
+    cardSuite: "Spades"
+    CardImage:
+  }
+
+]
